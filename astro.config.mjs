dc3d770c335f39c80366a82df9b078fdaa051642
jeremiebowles <jeremie.bowles@gmail.com>
2026-01-31 16:44:18 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.beanfreaks.com',
-  integrations: [tailwind(), sitemap()],
+  site: "https://jeremiebowles.github.io",
+  base: "/beanfreaks",
 });
+
