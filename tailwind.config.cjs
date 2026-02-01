a8a4fcc3deb15ad790d98531b39dd622cb62d1bb
Jeremie Bowles <12345678+jeremiebowles@users.noreply.github.com>
2026-02-01 15:40:04 +0000

    Replace site with known-good Astro + Tailwind + GitHub Pages setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
-  theme: { extend: {} },
-  plugins: [],
+  theme: {
+    extend: {}
+  },
+  plugins: []
 };
