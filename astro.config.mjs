caaed9beeeddff82334ed665169feeaaf152ebf5
Jeremie Bowles <12345678+jeremiebowles@users.noreply.github.com>
2026-02-01 09:24:02 +0000

    Fix base-path links for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://jeremiebowles.github.io",
   base: "/beanfreaks",
-
-  // avoid the default "_astro" folder entirely
   build: { assets: "assets" },
-
   integrations: [tailwind(), sitemap()],
 });
 
