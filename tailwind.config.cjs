70e88faad970806de858d862a2c8d44f53c91b55
Jeremie Bowles <12345678+jeremiebowles@users.noreply.github.com>
2026-02-01 17:50:58 +0000

    Add typography styling so pages look styled

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}", "./dist/**/*.html"],
+  theme: { extend: {} },
+  plugins: [require("@tailwindcss/typography")],
 };
