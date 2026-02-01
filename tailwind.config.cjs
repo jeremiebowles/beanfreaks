d2b08901f0406a07d033c95268243fb77cb5045c
Jeremie Bowles <12345678+jeremiebowles@users.noreply.github.com>
2026-02-01 14:26:19 +0000

    Fix Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,6 @@
-cat > tailwind.config.cjs <<'CJS'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
   theme: { extend: {} },
   plugins: [],
 };
-CJS
-
